bf79f245c7d4d175acd1a9987e9d32ba200664d7
Jamir Garro <132406066+Doki2007@users.noreply.github.com>
2026-05-22 09:55:13 -0500

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+
+        const sortedNums = nums.sort((a, b) => a - b);
+        const result = [];
+
+        for( let i = 0; i < sortedNums.length; i++ ) {
+            if( sortedNums[i] > 0 ) break;
+            if( sortedNums[i] === sortedNums[i - 1] ) continue;
+            let left = i + 1;
+            let right = sortedNums.length - 1;
+
+            while( left < right ) {
+                const sum = sortedNums[i] + sortedNums[left] + sortedNums[right];
+                if( sum > 0 ){
+                    right--;
+                } else if ( sum < 0 ){
+                    left++;
+                } else {
+                    result.push([sortedNums[i], sortedNums[left], sortedNums[right]])
+                    left++;
+                    while ( sortedNums[left] === sortedNums[left - 1]) left++;
+
+                }
+            } 
+        }
+        return result;
+    }
+}
